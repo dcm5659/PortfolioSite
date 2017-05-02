76271c39e8494c443322d34a3e55b132c5cb9a5a
DESKTOP-8U13VIS\Dan <dcm5659>
2017-05-02 17:28:48 -0400

    Full update all pages Removed extreneous files

--- scripts/myjs.js
+++ scripts/myjs.js
@@ -1,41 +1,10 @@
 
 $( document ).ready(function() {
 
-  $('#nmtp').hover(
-    function(){
-      $('#nmtptitle').css('color', '#93DEFF');
-    }, function(){
-      $('#nmtptitle').css('color', '#717B7F');
-    }
-  );
-
-  $('#snow').hover(
-    function(){
-      $('#snowtitle').css('color', '#93DEFF');
-    }, function(){
-      $('#snowtitle').css('color', '#717B7F');
-    }
-  );
-
-  matchX();
+ 
 });
 
 
 $(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 600) {
-            $("img").removeClass("rightCol").addClass("blueBG");
-            console.log("Done");
-    }
 
-    matchX();
 });
-
-function matchX() {
-      console.log("ran")
-      var ppos = $('#smiles').position();
-      console.log(ppos.left);
-      //console.log($('#smilestitle').left);
-      $('#smilestitle').position({left: ppos - 10});
-      
-};
